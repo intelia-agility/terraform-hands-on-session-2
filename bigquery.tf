63df35c268d6cde884ca6df8f1035e589d6d928a
Rohan Paithankar <80022518+rohanpaithankar-intelia@users.noreply.github.com>
2023-03-08 21:54:31 +1100

    Updated bigquery.tf

--- bigquery.tf
+++ bigquery.tf
@@ -1,5 +1,6 @@
 # Step 1: Create a BigQuery dataset config published module.
 # Step 2: Ensure that the dataset is created after the GCS bucket.
-# Step 2: Create multiple datasets using count.
-# Step 3: Create dataset using a resource toggle (count).
-# Step 4: Create a bigquery dataset in 2 different workspaces.
+# Step 3: Create multiple datasets using count.
+# Step 4: Create dataset using a resource toggle (count).
+# Step 5: Convert this into a child module.
+# Step 4: Create the resources in 2 different workspaces.
