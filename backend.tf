bd190dc4978315c3c57ba08593bf73d9e0948fc3
Rohan Paithankar <80022518+rohanpaithankar-intelia@users.noreply.github.com>
2023-03-09 10:54:33 +1100

    Updated backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 #terraform {
 #  backend "gcs" {
 #    bucket  = "terraform-hands-on-state-session2"
-#    prefix  = "terraform/session2/{YOUR_NAME}/"
+#    prefix  = "terraform/{YOUR_NAME}/"
 #  }
 #}
