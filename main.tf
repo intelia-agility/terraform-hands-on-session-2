c62e0dd7a5fe62f9f394d299f1c15dda62562576
Rohan Paithankar <80022518+rohanpaithankar-intelia@users.noreply.github.com>
2023-03-09 11:01:54 +1100

    Updated main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
-# Step 1: Create a BigQuery dataset config published module.
-# Step 2: Ensure that the dataset is created after the GCS bucket.
+# Step 1: Create a BigQuery dataset config published module. Refer: https://github.com/terraform-google-modules/terraform-google-bigquery/tree/master/examples/basic_bq
+# Step 2: Created another Bigquery Dataset and ensure that it is created after the first one.
 # Step 3: Create multiple datasets using count.
-# Step 4: Create dataset using a resource toggle (count).
+# Step 4: Create dataset using a resource toggle switch.
 # Step 5: Convert this into a child module.
 # Step 4: Create the resources in 2 different workspaces.
